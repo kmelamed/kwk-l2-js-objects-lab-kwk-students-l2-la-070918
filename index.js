2ec2f2cd5a166f2f97090480092a6bc9a707ec4c
kmelamed <kmelamed@buckley.org>
2018-07-13 22:45:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your solution in this file!
+
+const driver = {
+  name: 'Sam',
+  address: '1s'
+}
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+  let driver2 = { [key]: value }
+ //let driver2 =object.assign({key: value}, driver);
+ // return object.assign(driver2, driver);
+ //driver2 = object.assign({name: 'Same', address: '1s', key: value}, driver1);
+ return driver2;
+}
